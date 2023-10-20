4a69035c96d4107e810b71ed31601da4dd912ff0
kanagarajgv <gvkanagaraj@gmail.com>
2023-10-20 11:33:11 +0530

    firstSample

---
+++ src/org/client/Client.java
@@ -0,0 +1,7 @@
+package org.client;
+
+public class Client {
+	public void clientName() {
+		System.out.println("the client name is:SRC consultants");
+	}
+}
---
+++ src/org/company/Company.java
@@ -0,0 +1,13 @@
+package org.company;
+import org.client.Client;
+
+public class Company extends Client {
+	private void companyName() {
+		System.out.println("the company name is:ferro");
+	}
+public static void main(String[] args) {
+	Company c=new Company();
+	c.companyName();
+	c.clientName();
+}
+}
